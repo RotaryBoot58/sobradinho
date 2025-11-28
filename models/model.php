6b173e8365e1a8a78ea7c6d750a540a32c339864
RotaryBoot58 <petersoncraft20@proton.me>
2025-11-28 11:36:56 -0300

    models/model.php: removed abstract functions except create(), added  parameters to index(), query() and read(), added update(), read() and delete() methods

--- models/model.php
+++ models/model.php
@@ -7,21 +7,52 @@ abstract class Model
 	protected Database $database;
 	protected string $table_name;
 
+	abstract public function create(array $data): bool;
+
 	public function __construct(string $table_name)
 	{
 		$this->database = new Database();
 		$this->table_name = $table_name;
 	}
 
-	public function index(): array
+	public function index(array $fields = ['*']): array
 	{
-		return $this->database->query("SELECT * FROM {$this->table_name}")->fetchAll();
+		$fields = implode(', ', $fields);
+		return $this->database->query("SELECT {$fields} FROM {$this->table_name}")->fetchAll();
 	}
 
-	abstract public function create(array $data): bool;
-	abstract public function read(int $id): array;
-	abstract public function update(int $id): bool;
-	abstract public function delete(int $id): bool;
+	public function query(array $fields = ['*']): array
+	{
+		$fields = implode(', ', $fields);
+		return $this->database->query("SELECT {$fields} FROM {$this->table_name}")->fetch();
+	}
+
+	public function update(int $id, array $data)
+	{
+		$fields = [];
+		$values = [];
+		foreach($data as $key => $value)
+		{
+			$fields[] = "$key = :$key";
+			$values[$key] = $value;
+		}
+	
+		$values['id'] = $id;
+		$fields = implode(', ',$fields);
+
+		$this->database->execute("UPDATE {$this->table_name} SET {$fields} WHERE id = :id", $values);
+	}
+
+	public function read(array $fields = ['*'], int $id): array|false
+	{
+		$fields = implode(', ', $fields);
+		return $this->database->query("SELECT {$fields} FROM {$this->table_name} WHERE id = :id", ['id' => $id])->fetch();
+	}
+
+	public function delete(int $id): bool
+	{
+		return $this->database->execute("DELETE FROM {$this->table_name} WHERE id = :id", ['id' => $id]);
+	}
 
 	public function checkExistence(int $id): bool
 	{
