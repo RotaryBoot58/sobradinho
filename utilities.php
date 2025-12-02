34da523617532b6c20a74c0921a59b401ccf69c7
RotaryBoot58 <petersoncraft20@proton.me>
2025-12-02 15:51:53 -0300

    utilities.php: new file for helper functions

---
+++ utilities.php
@@ -0,0 +1,37 @@
+<?php
+
+function module(string $module): string
+{
+	return "../{$module}.php";
+}
+
+function model(string $model): string
+{
+	return "../models/{$model}.php";
+}
+
+function component(string $component): string
+{
+	return "../views/components/{$component}.php";
+}
+
+function view(string $view): string
+{
+	return "../views/{$view}.php";
+}
+
+function redirect(string $route)
+{
+	header("Location: http://localhost:8000/{$route}");
+	exit();
+}
+
+function printr(...$array)
+{
+	echo "<pre>";
+	foreach($array as $item)
+	{
+		print_r($item);
+	}
+	echo "</pre>";
+}
