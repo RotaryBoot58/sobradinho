47b8d9444451f2a0c58ef559e39ad04ea0aec1a6
RotaryBoot58 <petersoncraft20@proton.me>
2025-12-04 11:27:21 -0300

    enums/validation_messages.php: adicionado enum para validação de mensagens

---
+++ enums/validation_messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'required' => 'O campo é obrigatório.',
+	'text' => 'O campo não deve conter números ou caracteres especiais.',
+	'number' => 'O campo só deve conter números.',
+	'alpha_numerical' => 'O campo só deve conter números e letras.',
+	'email' => 'O campo deve ser um email válido.',
+	'in_array' => 'O campo não tem um valor válido.',
+	'exists' => 'O item não existe no banco de dados.'
+];
