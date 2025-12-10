6d15fe96dea7c5b5672452b8ede447fa8dba2245
RotaryBoot58 <petersoncraft20@proton.me>
2025-12-10 20:41:04 -0300

    views/service/index.php: overhaul

--- views/service/index.php
+++ views/service/index.php
@@ -3,54 +3,59 @@
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="/css/style.css">
-	<link rel="stylesheet" href="/css/home.css">
+	<link rel="stylesheet" href="/css/service/index.css">
 	<title>Sobradinho | Serviços</title>
 </head>
 <body>
-	<?php
-		session_start();
-		isset($_SESSION['success']) && renderSuccessBox($_SESSION['success']);
-		isset($_SESSION['error']) && renderErrorBox($_SESSION['error']);
-		session_destroy();
-	?>
-	<main>
-		<h1>Serviços</h1>
+	<aside>
+		<?php require component('components/nav.html', NO_EXT); ?>
 
 		<h2>Filtros</h2>
 		<form method="GET" action="/services">
-			<label>
-				Tipo:
-				<select name="type">
-					<option value="">Selecionar tipo</option>
-					<option value="Instalação">Instalação</option>
-					<option value="Manutenção">Manutenção</option>
-					<option value="Retirada">Retirada</option>
-					<option value="Upgrade">Upgrade</option>
-					<option value="Troca de endereço">Troca de endereço</option>
-				</select>
-			</label>
+			<label for="type">Tipo:</label>
+			<select name="type" id="type">
+				<option value="">Selecionar tipo</option>
+				<option value="Instalação">Instalação</option>
+				<option value="Manutenção">Manutenção</option>
+				<option value="Retirada">Retirada</option>
+				<option value="Upgrade">Upgrade</option>
+				<option value="Troca de endereço">Troca de endereço</option>
+			</select>
 
-			<label>
-				Técnico:
-				<select name="technic">
-					<option value="">Selecionar tipo</option>
-					<option value="Marcelo">Marcelo</option>
-				</select>
-			</label>
+			<br>
+
+			<label for="technic">Técnico:</label>
+			<select name="technic" id="technic">
+				<option value="">Selecionar tipo</option>
+				<option value="Marcelo">Marcelo</option>
+			</select>
 
 			<fieldset>
 				<legend>Data</legend>
-				
-				<span>De</span>
-				<input type="date" name="start_date">
-				
-				<span>Até</span>
-				<input type="date" name="end_date" value=<?= date('Y-m-d'); ?>>
+
+				<label for="start_date">De:</label>
+				<input type="date" name="start_date" id="start_date">
+
+				<br>
+
+				<label for="end_date">Até:</label>
+				<input type="date" name="end_date" id="end_date">
 			</fieldset>
 
 			<button type="submit">Filtrar</button>
 		</form>
-		
+
+		<?php
+			session_start();
+			isset($_SESSION['success']) && renderSuccessBox($_SESSION['success']);
+			isset($_SESSION['error']) && renderErrorBox($_SESSION['error']);
+			session_destroy();
+		?>
+	</aside>
+
+	<main>
+		<h1>Serviços</h1>
+
 		<table>
 			<thead>
 				<tr>
@@ -65,30 +70,24 @@
 				</tr>
 			</thead>
 			<tbody>
-				<?php
-					foreach($services as $service)
-					{
-						echo <<<HTML
-							<tr>
-								<td>$service[2]</td>
-								<td>$service[1]</td>
-								<td>$service[3]</td>
-								<td>$service[4] - $service[5]</td>
-								<td>$service[6] - $service[7]</td>
-								<td>$service[8]</td>
-								<td>$service[9] - $service[10]</td>
-								<td>
-									<a href="/service/update?id=$service[0]">Editar</a>
-
-									<form method="post" action="service/delete">
-										<input type="hidden" name="id" value="$service[0]">
-										<button type="submit">Apagar</button>
-									</form>
-								</td>
-							</tr>
-						HTML;
-					}
-				?>
+			<?php foreach($services as $service): ?>
+				<tr>
+					<td><?=$service[2]?></td>
+					<td><?=$service[1]?></td>
+					<td><?=$service[3]?></td>
+					<td><?="$service[4] - $service[5]"?></td>
+					<td><?="$service[6] - $service[7]"?></td>
+					<td><?=$service[8]?></td>
+					<td><?="$service[9] - $service[10]"?></td>
+					<td>
+						<a href=<?="/service/update?id=$service[0]"?>>Editar</a>
+						<form method="post" action="service/delete">
+							<input type="hidden" name="id" value=<?=$service[0]?>>
+							<button type="submit">Apagar</button>
+						</form>
+					</td>
+				</tr>
+			<?php endforeach; ?>
 			</tbody>
 		</table>
 	</main>
