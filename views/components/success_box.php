e712ac32c30adfc4501352a13e0eaf705fc8feaf
RotaryBoot58 <petersoncraft20@proton.me>
2025-12-13 02:09:28 -0300

    views/components/error_box.php: fixed bug to properly show message instead of 'Array'

--- views/components/success_box.php
+++ views/components/success_box.php
@@ -1,8 +1,6 @@
 <h2>Sucesso</h2>
-<div id="success">
-	<?php foreach($item as $message): ?>
-	<p>
-		<?= $message ?>
-	</p>
-	<?php endforeach; ?>
-</div>
+<ul id="success">
+	<li>
+		<?= $item[0]; ?>
+	</li>
+</ul>
