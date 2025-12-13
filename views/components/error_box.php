64375b90b9396a9b2a58f51ce3bd6430b14a97ee
RotaryBoot58 <petersoncraft20@proton.me>
2025-12-13 02:11:52 -0300

    views/components/error_box.php: now renders items that are not array properly and added a way to render name of items correctly

--- views/components/error_box.php
+++ views/components/error_box.php
@@ -2,13 +2,32 @@
 <ul id="error">
 	<?php
 	$field_labels = [
+		'numero' => 'Número',
+		'rua' => 'Rua',
+		'bairro' => 'Bairro',
+		'cidade' => 'Cidade',
+		'cep' => 'CEP',
 		'code' => 'Código',
 		'technic' => 'Técnico',
-		'onu_usage' => 'Uso de onu'
+		'onu_usage' => 'Uso de onu',
+		'database' => 'Banco de dados'
 	];
 
 	foreach($item as $field => $messages)
 	{
+		if(!is_array($messages))
+		{
+			$label = $field_labels[$field] ?? $field; 
+			$formatted_field = htmlspecialchars($label);
+			$formatted_message = htmlspecialchars($message);
+			
+			echo <<<HTML
+				<li>
+					<strong>$formatted_field</strong>: $formatted_message
+				</li>
+			HTML;
+		}
+	
 		foreach($messages as $message)
 		{
 			$label = $field_labels[$field] ?? $field; 
